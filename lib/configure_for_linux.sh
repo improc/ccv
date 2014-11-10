fcd75fe9f770d950fff203807a394a8f268b2285
Radek Benes <radek.benes@innovatrics.com>
2014-11-10 09:26:21 +0100

    configuration updates

--- lib/configure_for_linux.sh
+++
@@ -1,5 +0,0 @@
-rm config.mk
-rm makefile
-cp config.mk_linux.in config.mk
-cp makefile_preconfigured makefile 
-make
--- lib/configure_for_win.bat
+++ lib/configure_for_win.bat
@@ -2,4 +2,3 @@ del config.mk
 del makefile
 copy config.mk_mingw.in config.mk
 copy makefile_preconfigured makefile
-make
